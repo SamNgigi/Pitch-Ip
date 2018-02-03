e5d57d79a8a1c16aae9fb163e3cdeda1da7e0751
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-02-03 10:27:08 +0300

    Added auth and profile templates. Added tests to manager.py

--- manager.py
+++ manager.py
@@ -3,7 +3,7 @@ from app import create_app, db
 # f/rom app.models import User, Review
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
-# , User=User, Review=Review
+
 # Creating the app instance
 app = create_app('development')
 
@@ -12,6 +12,17 @@ migrate = Migrate(app, db)
 
 manager.add_command('server', Server)
 manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def test():
+    """
+    Running the unit tests.
+    """
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    # , User=User, Review=Review
 
 
 @manager.shell
